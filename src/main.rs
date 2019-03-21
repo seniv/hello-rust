3b8f8971925594ec0a87a47d71fb1b69284d4e62
Ivan Seniv <seniv97@gmail.com>
2019-03-21 23:34:29 +0200

    set port, address or path of the server in params

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
 extern crate actix_web;
-use actix_web::{server, App, fs};
+extern crate clap;
 
-fn create_app() -> App {
-    App::new()
-        .handler(
-            "/",
-            fs::StaticFiles::new(".")
-                .unwrap()
-                .show_files_listing()
-        )
-}
+use actix_web::{server, App, fs};
+use clap::{Arg};
 
 fn main() {
-    let port = 3000;
-    let host = format!("127.0.0.1:{}", port);
+    let matches = clap::App::new("Static HTTP server")
+        .author("Ivan Seniv <senivq@gmail.com>")
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .help("Sets port of the server")
+            .takes_value(true))
+        .arg(Arg::with_name("address")
+            .short("a")
+            .long("address")
+            .help("Sets address of the server")
+            .takes_value(true))
+        .arg(Arg::with_name("path")
+            .help("Sets path")
+            .index(1))
+        .get_matches();
 
-    println!("Starting the server at http://{}...", host);
+    let address = matches.value_of("address").unwrap_or("127.0.0.1");
+    let port = matches.value_of("port").unwrap_or("3000");
+    let path = String::from(matches.value_of("path").unwrap_or("."));
 
-    server::new(create_app)
+    let host = format!("{}:{}", address, port);
+
+    println!("Starting the server at http://{} in folder \"{}\"", host, path);
+
+    server::new(move || App::new()
+        .handler(
+            "/",
+            fs::StaticFiles::new(&path)
+                .unwrap()
+                .show_files_listing()
+        ))
         .bind(host)
         .unwrap()
         .run();
