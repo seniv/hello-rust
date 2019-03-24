ccbbf4878ead7fad09cedeca085c0dde655df997
Ivan Seniv <seniv97@gmail.com>
2019-03-24 20:20:00 +0200

    ability to select which address to bind

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@ extern crate clap;
 use actix_web::{server, App, fs};
 use clap::{Arg};
 
+fn select_address() -> String {
+    let addrs = get_if_addrs::get_if_addrs().unwrap();
+
+    println!("Please, enter index of address you want to bind");
+    let mut iter_index = 0;
+    addrs.iter().for_each(|addr| {
+        println!("[{}]: {}", iter_index, addr.ip());
+        iter_index += 1;
+    });
+
+    let mut index = String::new();
+    std::io::stdin().read_line(&mut index).unwrap();
+    let index: usize = index.trim().parse().unwrap();
+
+    addrs[index].ip().to_string()
+}
+
 fn main() {
     let matches = clap::App::new("Static HTTP server")
         .author("Ivan Seniv <senivq@gmail.com>")
@@ -17,18 +34,18 @@ fn main() {
             .long("address")
             .help("Sets address of the server")
             .takes_value(true))
-        .arg(Arg::with_name("local")
-            .short("l")
-            .long("local")
-            .help("Try to bind local address"))
+        .arg(Arg::with_name("select")
+            .short("s")
+            .long("select")
+            .help("Select which address you want to bind"))
         .arg(Arg::with_name("path")
             .help("Sets path")
             .index(1))
         .get_matches();
 
-    let use_local = matches.is_present("local");
-    let address = if use_local {
-        get_if_addrs::get_if_addrs().unwrap()[0].ip().to_string()
+    let display_select = matches.is_present("select");
+    let address = if display_select {
+        select_address()
     } else {
         matches.value_of("address").unwrap_or("127.0.0.1").to_string()
     };
